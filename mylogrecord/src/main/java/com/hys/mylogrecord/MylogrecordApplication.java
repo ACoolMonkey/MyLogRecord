1aa35b9536c9b5f6a14bd2ce7bbfdcf2005c30f5
Robert Hou <houyishuang950215@qq.com>
2022-04-26 13:21:48 +0800

    初始提交

---
+++ mylogrecord/src/main/java/com/hys/mylogrecord/MylogrecordApplication.java
@@ -0,0 +1,13 @@
+package com.hys.mylogrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylogrecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MylogrecordApplication.class, args);
+    }
+
+}
---
+++ mylogrecord/src/test/java/com/hys/mylogrecord/MylogrecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hys.mylogrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylogrecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
