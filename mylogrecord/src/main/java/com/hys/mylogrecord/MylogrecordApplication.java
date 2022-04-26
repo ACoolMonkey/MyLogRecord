df8d732f22f9fb0b70408e49b74731c140d1baa6
Robert Hou <houyishuang950215@qq.com>
2022-04-26 19:07:18 +0800

    update

--- mylogrecord/src/main/java/com/hys/mylogrecord/MylogrecordApplication.java
+++ mylogrecord/src/main/java/com/hys/mylogrecord/MylogrecordApplication.java
@@ -2,8 +2,10 @@ package com.hys.mylogrecord;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MylogrecordApplication {
 
     public static void main(String[] args) {
