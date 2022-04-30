dc340dad4dd2a392674a3ba869855135b611563f
MonkeyOneCool <houyishuang950215@qq.com>
2022-04-30 23:54:35 +0800

    reflections version upgrade

--- mylogrecord/src/main/java/com/hys/mylogrecord/config/LogRecordInitializer.java
+++ mylogrecord/src/main/java/com/hys/mylogrecord/config/LogRecordInitializer.java
@@ -5,8 +5,9 @@ import com.hys.mylogrecord.customfunction.MyLogRecordFunction;
 import com.hys.mylogrecord.customfunction.MyLogRecordSnapshotFunction;
 import com.hys.mylogrecord.parse.util.LogRecordParseUtils;
 import org.reflections.Reflections;
-import org.reflections.scanners.MethodAnnotationsScanner;
-import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.Scanners;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -31,7 +32,10 @@ public class LogRecordInitializer implements InitializingBean {
     @Override
     public void afterPropertiesSet() {
         //动态模板初始化
-        Reflections reflections = new Reflections("com.hys", new MethodAnnotationsScanner(), new SubTypesScanner());
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .forPackages("com.hys")
+                .filterInputsBy(new FilterBuilder().includePackage("com.hys"))
+                .setScanners(Scanners.MethodsAnnotated, Scanners.SubTypes));
         Set<Method> methods = reflections.getMethodsAnnotatedWith(MyLogRecord.class);
         for (Method method : methods) {
             MyLogRecord annotation = method.getAnnotation(MyLogRecord.class);
