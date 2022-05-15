ec78a85cc8415cc633a71aafc5a49636071e1074
MonkeyOneCool <houyishuang950215@qq.com>
2022-05-15 18:55:52 +0800

    update

--- mylogrecord/src/main/java/com/hys/mylogrecord/aop/annotation/CustomSnapshotFunction.java
+++ mylogrecord/src/main/java/com/hys/mylogrecord/aop/annotation/CustomSnapshotFunction.java
@@ -2,11 +2,16 @@ package com.hys.mylogrecord.aop.annotation;
 
 import com.hys.mylogrecord.customfunction.MyLogRecordSnapshotFunction;
 
+import java.lang.annotation.*;
+
 /**
  * 支持快照缓存，需要实现{@link MyLogRecordSnapshotFunction}接口
  *
  * @author Robert Hou
  * @since 2022年04月27日 17:56
  **/
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface CustomSnapshotFunction {
 }
